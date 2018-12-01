f97e8b1b445f966a4dca3201695e58180d5fcfbb
Carolyn Olsen <8650340+CarolynOlsen@users.noreply.github.com>
2018-12-01 15:47:03 -0600

    Update QueryPostgreSQL.py

--- QueryPostgreSQL.py
+++ QueryPostgreSQL.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 
 # get database login information from environment file
-load_dotenv('C:/Users/OLS14/py_secrets/.env')
-SERVER = os.environ.get('edna_mi_postgresql_server')
-USER = os.environ.get('edna_mi_postgresql_user')
-PASSWORD = os.environ.get('edna_mi_postgresql_pw')
-DATABASE = os.environ.get('edna_mi_postgresql_db')
+load_dotenv('[PATH]/.env')
+SERVER = os.environ.get('my_postgresql_server')
+USER = os.environ.get('my_postgresql_user')
+PASSWORD = os.environ.get('my_postgresql_pw')
+DATABASE = os.environ.get('my_postgresql_db')
 
 # create database connection
 cxn = psycopg2.connect(host=SERVER, database=DATABASE, user=USER, password=PASSWORD)
